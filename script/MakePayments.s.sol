7f4daab3b64d36d1225b580f6e25e67d2d461882
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2025-03-02 14:33:29 +0000

    Intitial setup

---
+++ script/MakePayments.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import {Script} from "forge-std/Script.sol";
+
+import {ISafe} from "../src/dependencies/ISafe.sol";
+
+
+
+contract MakePayments is Script {
+
+    function run() public {
+        
+
+
+
+    }
+}
---
+++ src/dependencies/Enum.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+library Enum {
+
+    enum Operation {
+        Call,
+        DelegateCall
+    }
+
+}
